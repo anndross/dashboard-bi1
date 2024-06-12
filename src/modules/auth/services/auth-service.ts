ffa4e7098433ebcfda43ed9677214008469af4df
Andrey Soares de Oliveira <andrey.soares@thealfred.com.br>
2024-06-12 12:49:15 -0300

    fix: undefined secret

--- src/modules/auth/services/auth-service.ts
+++ src/modules/auth/services/auth-service.ts
@@ -1,7 +1,7 @@
 import * as jose from 'jose'
 import { cookies } from 'next/headers'
 
-const secret = new TextEncoder().encode(process.env.AUTH_SECRET)
+const secret = new TextEncoder().encode(process.env.AUTH_SECRET || '1o3nrfoadnfanofaopwens')
 
 async function openSessionToken(token: string) {
   const { payload } = await jose.jwtVerify(token, secret)
