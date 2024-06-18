88565bf14a6957532db1798a25ea418d6117818d
Andrey Soares de Oliveira <andrey.soares@thealfred.com.br>
2024-06-18 10:14:31 -0300

    fix: remove router

--- src/modules/auth/components/logout-button.tsx
+++ src/modules/auth/components/logout-button.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { Button } from "@tremor/react";
-import { useRouter } from "next/router";
 import { MouseEvent } from "react";
 
 export function LogoutButton() {
-  const router = useRouter();
-
   async function handleLogout(event: MouseEvent<HTMLElement>) {
     event.preventDefault();
-    router.push("/login");
 
     const response = await fetch("https://dashboard-bi1.vercel.app/api/logout");
     const message = await response.json();
