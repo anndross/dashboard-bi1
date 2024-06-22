a543a71e6d01e5d45c009892129d18f5a7c6f67e
Andrey Soares de Oliveira <andrey.soares@thealfred.com.br>
2024-06-22 11:53:52 -0300

    style: filters get width 100%

--- src/components/Filters/FiltersRoot.tsx
+++ src/components/Filters/FiltersRoot.tsx
@@ -6,7 +6,7 @@ interface FiltersRootProps {
 
 export function FiltersRoot({ children }: FiltersRootProps) {
   return (
-    <div className="max-md:top-[100vh] shadow-sm z-20 w-fulls h-12 fixed top-20 bg-white px-10 flex items-center justify-end gap-4">
+    <div className="max-md:top-[100vh] shadow-sm z-20 w-fulls h-12 fixed top-20 bg-white px-10 flex items-center justify-end gap-4 w-full">
       {children}
     </div>
   );
